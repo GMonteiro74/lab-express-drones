3f43a83f76f02a3c9afea7472879f1c83fa3786c
monteiro74 <monteiro74@protonmail.com>
2021-09-06 16:11:20 +0100

    solved lab

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,16 @@
 // Iteration #1
+require("../db"); 
+
+const Drone = require('../models/Drone.model');
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+Drone.create(drones).then((dronesFromDb) => {
+    console.log(`drones created: ${drones.length}`);
+}).catch( err => {
+    console.log('Error', err);
+})
